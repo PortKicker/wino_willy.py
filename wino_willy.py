37dd1ac8f5960d5af527d8f434c60cb7547523d0
PortKicker <PortKicker@protonmail.com>
2019-06-08 14:26:41 -0700

    removed extra variable

--- wino_willy.py
+++ wino_willy.py
@@ -74,23 +74,6 @@ def intro():
 
     pick = True
 
-    res =['Hey guy, Let\'s pick an actual menu option.',
-'I thought Wino Willy was the drunk here, pick an option.',
-'You can\'t have your pudding if you don\'t pick a menu option.',
-'There are three menu options, how hard can it be to choose one.',
-'Help, Play or Quit, not a huge selection.',
-'If you type Play, you can make all of these mistakes in the game.',
-'If you type Help, it will tell you how to play, but not how to fix your life.',
-'if you type Quit, you can go back to your life. I know, not very appealing.',
-'"Only cool kids play Wino Willy." - Steve Jobs',
-'"This is the best game ever invented!" - Barack Obama',
-'"only people with huge pp choose a menu option." - ur mom',
-'Let\'s try something new, like picking an option from this menu.',
-'If you can doge a wrench, you can type in a valid menu option.',
-'For good time, call 555-pick-a-menu-option.',
-'Help, Play or Quit. Not Waste, My or Time.',
-'"ArgHSsss...*Barf*....AgarSfffg...." - Wino Willy']
-
     while pick:
 
        option = input('8===D ')
@@ -240,7 +223,7 @@ RIGHT : 'RIGHT', 'EAST', 'E',
          ####### MAP #######
 
       #     a1    a2   a3
-      #      ____________
+      #      ____________d
       #      |   |   |   | a3
       #      |___|___|___|
       #      |   |   |   | b3
